d16746aeb190a40a751d2b23b164284287da3f8e
simonpatrat <simonpatrat@users.noreply.github.com>
2016-04-06 22:35:41 +0200

    Changed styles-customizer color switch method

--- HTML/Light/assets/js/style-customizer.js
+++ HTML/Light/assets/js/style-customizer.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    var $customizerOverlay = $('<div class="customizer-overlay" style="position: absolute; top:0; left:0; right:0; bottom:0; width: 100%; height: 100%; background: #fff; z-index: 2000;"><div class="cus-loader" style="width: 176px; height: 176px; position: absolute; top: 50%; left: 50%; margin-left: -88px; margin-top: -88px; background: url(assets/img/loading.gif) no-repeat center;"></div></div>');
+
     $('.toggle-style-switcher').click(function() {
         
         $('#style-switcher').toggleClass('visible');
@@ -28,26 +30,34 @@ $(document).ready(function() {
             $('.color-list li').removeClass('selected');
             $('.color-list').on('click', 'li[data-style='+colorOnLoad+']').addClass('selected'); 
             if(colorOnLoad != 'default') {
-                $styleTag.attr('href', 'styles'+'-'+colorOnLoad+'.css');
+                $('<link rel="stylesheet" data-style-added="true" href="styles'+'-'+colorOnLoad+'.css">').insertAfter($styleTag);
+                //$styleTag.attr('href', 'styles'+'-'+colorOnLoad+'.css');
                 
             }else {
-                $styleTag.attr('href', 'styles.css');
+                $('*[data-style-added="true"]').remove();
+                //$styleTag.attr('href', 'styles.css');
             }       
         }else {
-            $styleTag.attr('href', 'styles.css');
+            $('*[data-style-added="true"]').remove();
+            //$styleTag.attr('href', 'styles.css');
         }
 
         // Color switcher
         $('.color-list li').on('click', function() {
+            
+            $customizerOverlay.prependTo('body');
 
             var colorStyle = $(this).data('style');
             localStorage.setItem("color_theme", colorStyle);
 
             if(colorStyle != 'default') {
-                $styleTag.attr('href', 'styles'+'-'+colorStyle+'.css');
+                //$styleTag.attr('href', 'styles'+'-'+colorStyle+'.css');
+                $('*[data-style-added="true"]').remove();
+                $('<link rel="stylesheet" data-style-added="true" href="styles'+'-'+colorStyle+'.css">').insertAfter($styleTag);
                 
             }else {
-                $styleTag.attr('href', 'styles.css');
+                $('*[data-style-added="true"]').remove();
+                //$styleTag.attr('href', 'styles.css');
             }
             $('body').attr('data-color-style', colorStyle);
             $('.color-list li').removeClass('selected');
@@ -55,6 +65,14 @@ $(document).ready(function() {
             
             $(document).trigger('stylesChanged');
 
+            setTimeout(function() {
+                    $customizerOverlay.fadeOut(1000, function() {
+                    
+                        $(this).remove();
+                    
+                    });
+
+            }, 1200);
 
             return false;
         }); 
